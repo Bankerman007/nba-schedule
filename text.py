b7b21c4382e4a0c49bb7c56442fb0fefe5315178
Scott Wright <scottwright.qrf@gmail.com>
2022-03-01 10:42:35 -0600

    changes

--- text.py
+++ text.py
@@ -6,11 +6,14 @@ def sms_all(scraper):
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
     scraper = str(scraper)
-    message = client.messages \
-                    .create(
-                    body= scraper,
-                    from_='+16467989631',
-                    to= ['+18475323886','+12242411079'],
-                        )
-        
-    print(message.sid)
+    numbers = ('8475323886','8475323886')
+    for i in numbers:
+        send = i
+        message = client.messages \
+                        .create(
+                        body= scraper,
+                        from_='+16467989631',
+                        to= '+1'+ send,
+                            )
+            
+        print(message.sid)
